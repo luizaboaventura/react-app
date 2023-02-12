b23e11ee96f4546323c615b9f9762a2f75e9b9c9
Luiza Boaventura Mendonça <110929562+luizaboaventura@users.noreply.github.com>
2023-02-12 18:57:52 -0300

    Add files via upload

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+
+export default function Card(props) {
+
+   return (
+      <div className='card'>
+         <strong>{props.name}</strong>
+         <small>{props.time}</small>
+      </div>
+   )
+}
